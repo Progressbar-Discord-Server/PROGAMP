f63ac3ed141237cf79db49e7b1a1e632fa6008e1
stuartt_mcoded <34777184+RealMCoded@users.noreply.github.com>
2022-10-30 12:30:56 -0400

    Create config.template.js

---
+++ config.template.js
@@ -0,0 +1,27 @@
+module.exports = {
+    app: {
+        token: '',
+        playing: 'Progressbar95',
+        global: false,
+        guild: ''
+    },
+
+    opt: {
+        DJ: {
+            enabled: false,
+            roleName: '',
+            commands: []
+        },
+        maxVol: 100,
+        leaveOnEnd: true,
+        loopMessage: false,
+        spotifyBridge: true,
+        defaultvolume: 75,
+        discordPlayer: {
+            ytdlOptions: {
+                quality: 'highestaudio',
+                highWaterMark: 1 << 25
+            }
+        }
+    }
+};
