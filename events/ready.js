b33d85bbdca25c8dba5791b32e16f4bd07a55a5c
stuartt_mcoded <34777184+RealMCoded@users.noreply.github.com>
2022-10-30 12:30:48 -0400

    Listening to activity type

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,6 @@
+const { ActivityType } = require('discord.js');
+
 module.exports = async (client) => {
     console.log(`Logged to the client ${client.user.username}\n-> Ready on ${client.guilds.cache.size} servers for a total of ${client.users.cache.size} users`);
-    client.user.setActivity(client.config.app.playing);
-
-    
+    client.user.setActivity(client.config.app.playing, { type: ActivityType.Listening });
 };
